62e28ab629cf7ca160c14fcce35e1c24d361f2c6
asm128 <pabloarielz@outlook.com>
2019-07-20 23:32:37 -0300

    + Restored databases broken by fucking git.

---
+++ blitter_db/all.bat
@@ -0,0 +1,33 @@
+reason website.json     0 
+reason website.json     1 
+reason website.json     0 MjAwOS0yMDE5IChjKSBUaGUgRHJhZ29u
+reason website.json     1 MjAwOS0yMDE5IChjKSBUaGUgRHJhZ29u
+reason publisher.json   0 
+reason publisher.json   1 
+reason publisher.json   0 MjAwOS0yMDE5IChjKSBUaGUgRHJhZ29u
+reason publisher.json   1 MjAwOS0yMDE5IChjKSBUaGUgRHJhZ29u
+reason company.json     0 
+reason company.json     1 
+reason company.json     0 MjAwOS0yMDE5IChjKSBUaGUgRHJhZ29u
+reason company.json     1 MjAwOS0yMDE5IChjKSBUaGUgRHJhZ29u
+reason user.json        0 
+reason user.json        1 
+reason user.json        0 MjAwOS0yMDE5IChjKSBUaGUgRHJhZ29u
+reason user.json        1 MjAwOS0yMDE5IChjKSBUaGUgRHJhZ29u
+
+season website.json     5 0 
+season website.json     5 1 
+season website.json     5 0 MjAwOS0yMDE5IChjKSBUaGUgRHJhZ29u
+season website.json     5 1 MjAwOS0yMDE5IChjKSBUaGUgRHJhZ29u
+season publisher.json   5 0 
+season publisher.json   5 1 
+season publisher.json   5 0 MjAwOS0yMDE5IChjKSBUaGUgRHJhZ29u
+season publisher.json   5 1 MjAwOS0yMDE5IChjKSBUaGUgRHJhZ29u
+season company.json     5 0 
+season company.json     5 1 
+season company.json     5 0 MjAwOS0yMDE5IChjKSBUaGUgRHJhZ29u
+season company.json     5 1 MjAwOS0yMDE5IChjKSBUaGUgRHJhZ29u
+season user.json        5 0	 
+season user.json        5 1 
+season user.json        5 0 MjAwOS0yMDE5IChjKSBUaGUgRHJhZ29u
+season user.json        5 1 MjAwOS0yMDE5IChjKSBUaGUgRHJhZ29u
